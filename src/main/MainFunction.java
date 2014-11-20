8f102ad89198319c96765d0ecb42c3b9a39ebca5
chenjunren0406 <junren_chen@yahoo.com>
2014-11-21 00:58:56 +0800

    behavior restriction

--- src/main/MainFunction.java
+++ src/main/MainFunction.java
@@ -6,7 +6,13 @@ import test.simple.MainFrame;
 
 public class MainFunction {
 	public static void main(String[] args) {
-		String s = "www.google.com";
+		System.setSecurityManager(new SecurityManager());
+		try{
+		String s = "www.rice.edu";
 	    MainFrame m = new MainFrame(s, OS.isLinux(), false);
+		}
+		catch(Throwable e){
+			System.out.println(e.toString());
+		}
 	  }
 }
