75ae9f286983b2b8292258b1db3a7c1927612c3f
chenjunren0406 <junren_chen@yahoo.com>
2014-11-22 06:07:34 +0800

    restriction for app

--- src/main/MainFunction.java
+++ src/main/MainFunction.java
@@ -1,18 +1,21 @@
 package main;
 
+import java.io.File;
+
 import org.cef.OS;
 
 import test.simple.MainFrame;
 
 public class MainFunction {
 	public static void main(String[] args) {
-		System.setSecurityManager(new SecurityManager());
+		SecurityManager sm = new SecurityManager();
+		System.setSecurityManager(sm);
 		try{
 		String s = "www.rice.edu";
 	    MainFrame m = new MainFrame(s, OS.isLinux(), false);
 		}
 		catch(Throwable e){
-			System.out.println(e.toString());
+			e.printStackTrace();
 		}
 	  }
 }
